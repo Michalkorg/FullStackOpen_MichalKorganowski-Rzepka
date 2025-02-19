4e8d833d3385146f1b8cd12244e53705d4d5c194
Michalkorg <124426915+Michalkorg@users.noreply.github.com>
2025-02-19 23:20:13 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,51 @@
-import { useState, useEffect } from 'react';
-import Form from './components/Form';
-import Numbers from './components/Numbers';
-import Notification from './components/Notification';
+import Courses from './components/Courses';
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  const [message, setMessage] = useState(null); // Stan dla wiadomości
-  const [newName, setNewName] = useState(''); // Stan dla nowego imienia
-  const [newNumber, setNewNumber] = useState(''); // Stan dla nowego numeru
-  const [filtered, setFiltered] = useState(''); // Stan dla filtrowania
-
-
-  const handleChange = (e, type) => {
-    switch (type) {
-      case 'name':
-        setNewName(e.target.value);
-        break;
-      case 'number':
-        setNewNumber(e.target.value);
-        break;
-      case 'filtered':
-        setFiltered(e.target.value);
-        break;
-      default:
-        break;
-    }
-  };
-  const messageHandler = (message, type) => {
-    setMessage({ message, type });
-    setTimeout(() => {
-      setMessage();
-    }, 2000);
-  };
-  const refreshInputs = () => {
-    setNewName('');
-    setNewNumber('');
-  };
-
-  const duplicate = (dup) => {
-    if (dup.number === newNumber)
-      return alert(`${newName} is already added to phonebook`);
-    if (
-      window.confirm(
-        `${newName} is already added to the phonebook, replace the old number with a new one?`
-      )
-    )
-      Persons.update(dup.id, { ...dup, number: newNumber })
-        .then((r) => {
-          setPersons(persons.map((p) => (p.id !== dup.id ? p : r)));
-          messageHandler(`Updated ${r.name}`, 'success');
-          refreshInputs();
-        })
-        .catch((err) => {
-          messageHandler(
-            `information of ${newName} has already been removed from server`,
-            'error'
-          );
-          refreshInputs();
-          setPersons(persons.filter((p) => p.id !== dup.id));
-        });
-  };
-  const addPerson = (e) => {
-    e.preventDefault();
-    const dup = persons.filter((p) => p.name === newName)[0];
- 
-    if (dup) {
-      duplicate(dup);
-    } else {
-      Persons.create({ name: newName, number: newNumber }).then((r) => {
-        setPersons(persons.concat(r));
-        messageHandler(`Added ${r.name}`, 'success');
-        refreshInputs();
-      }); 
-    }
-  };
-  const deletePerson = (id) => {
-    window.confirm(`Delete ${persons.find((p) => p.id === id).name}?`) &&
-      Persons.deleteAxios(id)
-        .then((r) => {
-          setPersons(persons.filter((p) => p.id !== id));
-          messageHandler(`Deleted the user successfully`, 'success');
-        })
-        .catch((err) => {
-          messageHandler(`The user already has been deleted`, 'error');
-          setPersons(persons.filter((p) => p.id !== id));
-        });
-  }; 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Notification message={message} />
-      <Form {...{ addPerson, filtered, handleChange, newName, newNumber }} />
-      {/* <p>debug:{newName}</p> */}
-      <Numbers {...{ persons, filtered, deletePerson }} />
-    </div>
-  );
+  return <Courses courses={courses} />;
 };
 
 export default App;
