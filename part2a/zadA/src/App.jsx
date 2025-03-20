f93d3af9538a750855cee67413f473ce81492603
Michał Korganowski <korganowskim@tlimc.local>
2025-03-20 15:21:56 +0100

    part2zadA

--- part2a/zadA/src/App.jsx
+++ part2a/zadA/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
